25cd1f6223a225063ce7d0b305a6c7e9efb4206b
Sai <github@saizai.com>
2014-12-30 14:28:49 -0700

    Ensure no variable name clashing

--- lib/devise_zxcvbn/model.rb
+++ lib/devise_zxcvbn/model.rb
@@ -14,22 +14,22 @@ module Devise
       private
 
       def not_weak_password
-        weak_words = []
+        zxcvbn_weak_words = []
         
         # User method results are saved locally to prevent repeat calls that might be expensive
         if self.respond_to? :email
-          email = self.email
-          weak_words += [email, *DeviseZxcvbn::EmailTokeniser.split(email)] 
+          local_email = self.email
+          zxcvbn_weak_words += [email, *DeviseZxcvbn::EmailTokeniser.split(email)] 
         end
         
         if self.respond_to? :weak_words
-          weak_words = self.weak_words
-          raise "weak_words must return an Array" unless (weak_words.is_a? Array)
-          weak_words += weak_words 
+          local_weak_words = self.weak_words
+          raise "weak_words must return an Array" unless (local_weak_words.is_a? Array)
+          zxcvbn_weak_words += local_weak_words 
         end
         
         
-        password_score = ::Zxcvbn.test(password, weak_words).score
+        password_score = ::Zxcvbn.test(password, zxcvbn_weak_words).score
         if password_score < min_password_score
           self.errors.add :password, :weak_password, score: password_score, min_password_score: min_password_score
           return false
